5f59fb8ad1bec838bbf9c314fb4b4efc2995dd4f
Kawsar-Hossain-318 <thekawsarhossain@gmail.com>
2021-11-12 00:21:17 +0600

    banner added

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -16,11 +16,12 @@ const Banner = () => {
         <Box style={bannerBg} sx={{ py: 16 }}>
             <Container>
                 <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                    <Grid item md={6} >
+                    <Grid item md={5} >
                         <img className="img-animation" src={drone} alt="" />
                     </Grid>
-                    <Grid item md={6} >
-                        <Typography variant="h2" sx={{ fontWeight: 'bold', color: 'text.secobdary' }}>DRONES CHANGE THE GAME</Typography>
+                    <Grid item md={7} sx={{ textAlign: 'start' }}>
+                        <Typography variant="h2" style={{ color: '#FFF', fontWeight: 'bold' }}>DRONES ARE <br /> CHANGE THE GAME</Typography>
+                        <Typography variant="body1" style={{ color: '#E7E6E1' }}>Drone is one of the most trending in the world right now. Drones have ultra-stable flight, and they can hover and perform different acrobatics in the air.When it comes to commercial drones for entertainment, they have a short control distance and can fly for just a few minutes.</Typography>
                     </Grid>
                 </Grid>
             </Container>
